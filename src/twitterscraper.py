f6f8e0ca7bf6d28d2ed80a33dce9ae2bb042cb12
BennyWestsyde <westfallg@allegheny.edu>
2021-05-05 18:00:19 -0400

    Finished twitterscraper and Prepare data

--- src/twitterscraper.py
+++ src/twitterscraper.py
@@ -5,6 +5,7 @@ import pandas as pd
 from bluebird import BlueBird
 from bluebird.scraper import BlueBird
 from io import StringIO
+from twitterUsernameviaUserID import getHandles as gH
 
 
 
@@ -28,113 +29,52 @@ class Twitter(object):
         }
         results_df = pd.DataFrame(columns=['index','created_at', 'full_text', 'user_id'])
         for tweet in BlueBird().search(query):
+            
             index += 1
-            res = {"created_at":tweet['created_at'],"full_text":tweet['full_text'],"user_id":tweet['user_id']}
-            print(res)
+            user = BlueBird().get_user_by_id(str(tweet['user_id']))
+            res = {"created_at":tweet['created_at'],"full_text":tweet['full_text'],"user_id":user['screen_name']}
             results_df = results_df.append(res, ignore_index=True)
-            print(index)
             if index == 50:
-                print("Tweets Found!!")
                 results_df.set_index('index')
                 results_df = results_df.drop('index', axis=1)
-                print(results_df.head)
-                
                 return results_df
         
 
 
     def search_hashtag(self, searching):
-        try:
-            os.remove('./data/results.csv')
-            os.remove('./data/temp.csv')
-        except:
-            print()
         index = 0
-
         query = {
             'fields': [
                 {'items': [searching], 'target':'hashtag'},
             ]
         }
-        results_df = pd.DataFrame()
+        results_df = pd.DataFrame(columns=['index','created_at', 'full_text', 'user_id'])
         for tweet in BlueBird().search(query):
+            
             index += 1
-            res = {"created_at":tweet['created_at'],"full_text":tweet['full_text'],"user_id":tweet['user_id']}
-            print(res)
-            results_df.append(res,ignore_index=True)
-            print(index)
+            user = BlueBird().get_user_by_id(str(tweet['user_id']))
+            res = {"created_at":tweet['created_at'],"full_text":tweet['full_text'],"user_id":user['screen_name']}
+            results_df = results_df.append(res, ignore_index=True)
             if index == 50:
-                print("Tweets Found!!")
-                print(results_df.head)
+                results_df.set_index('index')
+                results_df = results_df.drop('index', axis=1)
                 return results_df
-            # with open(self.user_results, 'a') as f:
-            #     df.to_csv(f, header=None, index=False)
-            # if index == 50:
-            #     dummy_file = self.user_results + '.bak'
-            #     with open(self.user_results, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
-            #         write_obj.write(self.col_names32 + '\n')
-            #         for line in read_obj:
-            #             write_obj.write(line)
-            #     os.remove(self.user_results)
-            #     os.rename(dummy_file, self.user_results)
-            os.remove("./data/temp.json")
-            return results_df
-
 
     def search_user(self, searching):
-        try:
-            os.remove('./data/results.csv')
-            os.remove('./data/temp.csv')
-        except:
-            print()
         index = 0
-
         query = {
             'fields': [
                 {'items': [searching], 'target':'from'},
             ]
         }
-        results_df = pd.DataFrame()
+        results_df = pd.DataFrame(columns=['index','created_at', 'full_text', 'user_id'])
         for tweet in BlueBird().search(query):
+            
             index += 1
-            res = {"created_at":tweet['created_at'],"full_text":tweet['full_text'],"user_id":tweet['user_id']}
-            print(res)
-            results_df.append(res,ignore_index=True)
-            print(index)
+            user = BlueBird().get_user_by_id(str(tweet['user_id']))
+            res = {"created_at":tweet['created_at'],"full_text":tweet['full_text'],"user_id":user['screen_name']}
+            results_df = results_df.append(res, ignore_index=True)
             if index == 50:
-                print("Tweets Found!!")
-                print(results_df.head)
+                results_df.set_index('index')
+                results_df = results_df.drop('index', axis=1)
                 return results_df
-
-# def main():
-#     try:
-#         os.remove('./data/results.csv')
-#         os.remove('./data/temp.csv')
-#     except:
-#         print()
-#     print("Welcome to the Fake News Dection Program! \n")
-#     print("Would you like to search by:\nkeyword\nhashtag\nuser")
-#     done = False
-#     while done == False:
-#         choice = input("keyword/hashtag/user: ")
-#         if choice == "keyword":
-#             search_term()
-#             done = True
-#         elif choice == "hashtag":
-#             search_hashtag()
-#             done = True
-#         elif choice == "user":
-#             search_user()
-#             done = True
-#         else:
-#             print("Sorry, Bad Input. Please Enter One of the Options Below")
-#             done = False
-#     try:
-#         os.remove('data/temp.json')
-#     except:
-#         print()
-
-
-# if __name__ == '__main__':
-#     # calls main function
-#     main()
