7e7c2a51a02f3b26ec5d44d63eac60f2a65e4562
Kiley <42876297+hewittk@users.noreply.github.com>
2021-05-09 22:03:11 -0400

    Configure file to properly import modules

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -1,10 +1,9 @@
 """Test module for sentiment.py"""
 import pytest
-import src.sentiment as sn
 from textblob import TextBlob
+import src.sentiment as sn
 
 def test_get_nouns():
     text = """"I am planning to run for President in the 2024 election with Jill Biden as my vice president."""
     blob = TextBlob(text)
     assert (len(blob.noun_phases)) == 6
-    
