716e9840a25efff0c8760f31fae51b8973db54ee
Kiley <42876297+hewittk@users.noreply.github.com>
2021-05-19 16:12:21 -0400

    Restore comments in remove_emoji

--- src/preparedata.py
+++ src/preparedata.py
@@ -23,8 +23,8 @@ class PrepareData(object):
 
     def remove_emoji(self, text):
             emoji_pattern = re.compile("["u"\U0001F600-\U0001F64F"  # emoticons
-                                       u"\U0001F300-\U0001F5FF"
-                                       u"\U0001F680-\U0001F6FF"
+                                       u"\U0001F300-\U0001F5FF"  # symbols
+                                       u"\U0001F680-\U0001F6FF"  # map/transport
                                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                                        u"\U00002702-\U000027B0"
                                        u"\U000024C2-\U0001F251"
