641038eb5f21f3f0e352043a5a8611147a331062
rachaelharris <rachaelharris@users.noreply.github.com>
2021-09-08 11:40:42 -0400

    adding pages to streamlit website

--- streamlit_web.py
+++ streamlit_web.py
@@ -1,20 +1,6 @@
 
-import sys
-import os
-import pickle
-from nltk.util import pr
-from src.seq_train import MachineBuilder as ms
-from numpy.lib.ufunclike import _fix_and_maybe_deprecate_out_named_y
-
 import streamlit as st
 import pandas as pd
-from src.twitterscraper import Twitter as ts
-from src.preparedata import PrepareData as prep
-import urllib
-import pandas as pd
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
-import webbrowser
 import src.installation
 from src.installation import installationPage
 import src.home
